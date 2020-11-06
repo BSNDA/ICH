2b75e4603f5f6647ffffd3eade51edc793ea2667
andycao125 <44188513+andycao125@users.noreply.github.com>
2020-11-06 20:07:09 +0800

    Update chaincode.go

--- sample/polychain/fabric-contract/hellopoly/chaincode.go
+++ sample/polychain/fabric-contract/hellopoly/chaincode.go
@@ -19,7 +19,7 @@ func setLogger(logInfo ...interface{}) {
 /**
  * @Author AndyCao
  * @Date 2020-11-4 18:27
- * @Description  合约初始化
+ * @Description  Contract initialization
  * @Param
  * @Return
  **/
@@ -32,7 +32,7 @@ func (t *HelloPoly) Init(stub shim.ChaincodeStubInterface) peer.Response {
 /**
  * @Author AndyCao
  * @Date 2020-11-4 18:27
- * @Description  合约调用
+ * @Description  Contract invocation
  * @Param
  * @Return
  **/
@@ -55,47 +55,47 @@ func (t *HelloPoly) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 /**
  * @Author AndyCao
  * @Date 2020-11-4 18:27
- * @Description 此方法用于对其它目标链进行跨链调用（此方法可自行定义）
+ * @Description This method is used for calling other target chains across the chain. (This method can be self-defined)
  * @Param
  * @Return
  **/
 func (t *HelloPoly) say(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	setLogger("The say method is called......")
 	defer setLogger("End called say method......")
-	// 参数检查
+	// Check configurations
 	if len(args) != 4 {
 		return shim.Error("Parameter error！！！")
 	}
-	// 保存数据
+	// Save data
 	if err:=stub.PutState(args[1],[]byte(args[2]));err!=nil{
 		return shim.Error(fmt.Sprintf("Failed to save data: %v", err))
 	}
-	// 构建跨链管理合约调用参数
+	// Build cross-chain management contract calling parameters
 	invokeArgs := make([][]byte, 6)
-	// 设置跨链管理合约被调用的方法
+	// Set the calling method of the cross-chain management contract
 	invokeArgs[0] = []byte("crossChain")
-	// 设置目标链在Poly网络中所对应的链应
+	// Set the chain corresponding to the target chain in Poly network 
 	invokeArgs[1] = []byte(args[0])
-	// 设置目标链应用合约地址，注：
-	// 1、目标链为fabric，则为应用合约的名称，如：mycc，目标链为fisco/eth/neo，则为应用合约地址，如：11...，前面不要加0x
-	// 2、传给跨链管理合约参数必须为hex.EncodeToString将bytes转换成16进制字符串，再转换成byte数组
+	// Set the target chain’s smart contract address, note：
+	// 1. If the target chain is fabric, then this is the name of the application contract, for example：mycc; if the target chain is fisco/eth/neo, then this is the name of the application contract, for example: 11..., don't add "0x" prefix.
+	// 2. The parameters passed to the cross-chain management contract must be EncodeToString converts bytes to hex strings and then transform to byte arrays.
 	if args[0]=="8"||args[0]=="9"{
 		invokeArgs[2] = []byte(hex.EncodeToString([]byte(args[1])))
 	}else{
 		invokeArgs[2] = []byte(args[1])
 	}
-	// 目标链应用合约方法
+        // Target chain’s application contract method
 	invokeArgs[3] = []byte("hear")
-	// 目标链应用合约所需要传递的跨链信息（注：传给跨链管理合约参数必须为hex.EncodeToString将bytes转换成16进制字符串，再转换成byte数组）
+	// Cross-chain information that target chain’s application contract needs to pass（Note: The parameters passed to the cross-chain management contract must be EncodeToString converts bytes to hex strings and then transform to byte arrays.）
 	invokeArgs[4] = []byte(hex.EncodeToString([]byte(args[2])))
-	// 应用合约的名字
+	// Name of the application contract
 	invokeArgs[5] = []byte(args[3])
-	// 调用跨链管理合约
+	// Calling cross-chain management contract
 	var resp = stub.InvokeChaincode(string("ccm"), invokeArgs, "")
 	if resp.Status != shim.OK {
 		return shim.Error(fmt.Sprintf("Failed to call the cross-chain management contract %s: %s", "ccm", resp.Message))
 	}
-	// 设置事件通知
+	// Set event notification
 	if err := stub.SetEvent("from_ccm", resp.Payload); err != nil {
 		return shim.Error(fmt.Sprintf("Event setting failed: %v", err))
 	}
@@ -106,19 +106,19 @@ func (t *HelloPoly) say(stub shim.ChaincodeStubInterface, args []string) peer.Re
 /**
  * @Author AndyCao
  * @Date 2020-11-4 18:28
- * @Description 此方法用于对其它目标链进行跨链调用（此方法可自行定义）
+ * @Description This method is used for calling other target chains across the chain. (This method can be self-defined)
  * @Param
  * @Return
  **/
 func (t *HelloPoly) hear(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	setLogger("The hear method is called......")
 	defer setLogger("End called hear method......")
-	// 参数检查
+	// Check parameters
 	if len(args) != 1 {
 		return shim.Error("Parameter error！！！")
 	}
 
-	// 保存源链所提交的跨链信息
+	// Save the cross-chain information committed by the source chain
 	if err:=stub.PutState("FABRIC_CROSS_CHAIN",[]byte(args[0]));err!=nil{
 		return shim.Error(fmt.Sprintf("Failed to save data: %v", err))
 	}
@@ -128,19 +128,19 @@ func (t *HelloPoly) hear(stub shim.ChaincodeStubInterface, args []string) peer.R
 /**
  * @Author AndyCao
  * @Date 2020-11-4 18:39
- * @Description  获取数据
+ * @Description  Get data
  * @Param
  * @Return
  **/
 func (t *HelloPoly) getValue(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	setLogger("The getValue method is called......")
 	defer setLogger("End getValue hear method......")
-	// 参数检查
+	// Check parameters
 	if len(args) != 1 {
 		return shim.Error("Parameter error！！！")
 	}
 
-	// 获取数据
+	// Get data
 	bytes,err:=stub.GetState(args[0])
 	if err!=nil{
 		return shim.Error(fmt.Sprintf("Get data error : %v", err))

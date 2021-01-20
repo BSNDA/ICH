cd507024407017d3d05bf95f10a04a8407883832
andycao125 <44188513+andycao125@users.noreply.github.com>
2021-01-20 18:12:30 +0800

    Add files via upload

---
+++ sample/polychain/online/hellopoly/chaincode.go
@@ -0,0 +1,144 @@
+package hellopoly
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+	"time"
+)
+
+type HelloPoly struct {
+}
+
+func setLogger(logInfo ...interface{}) {
+	t := time.Now()
+	fmt.Printf("%s  ->  %s\n", t.Format("2006-01-02 15:04:05.000"), logInfo)
+}
+
+/**
+ * @Author AndyCao
+ * @Date 2020-11-4 18:27
+ * @Description  Contract initialization
+ * @Param
+ * @Return
+ **/
+func (t *HelloPoly) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	setLogger("Start initializing init method......")
+	defer setLogger("End initialization of init method......")
+	return shim.Success(nil)
+}
+
+/**
+ * @Author AndyCao
+ * @Date 2020-11-4 18:27
+ * @Description  Contract invocation
+ * @Param
+ * @Return
+ **/
+func (t *HelloPoly) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	function, args := stub.GetFunctionAndParameters()
+	switch function {
+	case "say":
+		return t.say(stub, args)
+	case "hear":
+		return t.hear(stub, args)
+	case "getValue":
+		return t.getValue(stub, args)
+	default:
+		setLogger("Unavailable method!")
+		break
+	}
+	return shim.Error("Unavailable request!")
+}
+
+/**
+ * @Author AndyCao
+ * @Date 2020-11-4 18:27
+ * @Description This method is used for calling other target chains across the chain. (This method can be self-defined)
+ * @Param
+ * @Return
+ **/
+func (t *HelloPoly) say(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	setLogger("The say method is called......")
+	defer setLogger("End called say method......")
+	// Check configurations
+	if len(args) != 4 {
+		return shim.Error("Parameter error!!!")
+	}
+	// Save data
+	if err:=stub.PutState(args[1],[]byte(args[2]));err!=nil{
+		return shim.Error(fmt.Sprintf("Failed to save data: %v", err))
+	}
+	// Build cross-chain management contract calling parameters
+	ccArgs := []string{"crossChain",
+		args[0],
+		hex.EncodeToString([]byte(args[1])),
+		args[2],
+		hex.EncodeToString([]byte(args[3]))}
+	// Calling cross-chain management contract
+	var resp = stub.InvokeChaincode(string("ccm"), packArgs(ccArgs), "")
+	if resp.Status != shim.OK {
+		return shim.Error(fmt.Sprintf("Failed to call the cross-chain management contract %s: %s", "ccm", resp.Message))
+	}
+	// Set event notification
+	if err := stub.SetEvent("from_ccm", resp.Payload); err != nil {
+		return shim.Error(fmt.Sprintf("Event setting failed: %v", err))
+	}
+	setLogger(fmt.Sprintf("Successfully call the cross-chain management contract for cross-chain: (target chain ID: %d, target chain contract address: %x, cross-chain message: %s)", args[0], args[1], args[2]))
+	return shim.Success(nil)
+}
+
+/**
+ * @Author AndyCao
+ * @Date 2020-11-4 18:28
+ * @Description This method is used for calling other target chains across the chain. (This method can be self-defined)
+ * @Param
+ * @Return
+ **/
+func (t *HelloPoly) hear(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	setLogger("The hear method is called......")
+	defer setLogger("End called hear method......")
+	// Check parameters
+	if len(args) != 1 {
+		return shim.Error("Parameter error!!!")
+	}
+
+	// Save the cross-chain information committed by the source chain
+	if err:=stub.PutState("FABRIC_CROSS_CHAIN",[]byte(args[0]));err!=nil{
+		return shim.Error(fmt.Sprintf("Failed to save data: %v", err))
+	}
+	return shim.Success([]byte("SUCCESS"))
+}
+
+/**
+ * @Author AndyCao
+ * @Date 2020-11-4 18:39
+ * @Description  Get data
+ * @Param
+ * @Return
+ **/
+func (t *HelloPoly) getValue(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	setLogger("The getValue method is called......")
+	defer setLogger("End getValue hear method......")
+	// Check parameters
+	if len(args) != 1 {
+		return shim.Error("Parameter error!!!")
+	}
+
+	// Get data
+	bytes,err:=stub.GetState(args[0])
+	if err!=nil{
+		return shim.Error(fmt.Sprintf("Get data error : %v", err))
+	}
+	return shim.Success(bytes)
+}
+
+func packArgs(args []string) [][]byte {
+	r := [][]byte{}
+	for _, s := range args {
+		temp := []byte(s)
+		r = append(r, temp)
+	}
+	return r
+}

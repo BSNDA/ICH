1453e54007302190e1ca9509f545ce415061d244
Gaokanxue <gaokanxue@reddatetech.com>
2021-11-10 20:16:54 +0800

    Updated version
    Updated version 0.6.10

--- sample/polychain/wuhan_contracts/hellopoly/IEthCrossChainManagerProxy.sol
+++ sample/polychain/wuhan_contracts/hellopoly/IEthCrossChainManagerProxy.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.6.10;
 
 /**
  * @dev Interface of the EthCrossChainManagerProxy for business contract like LockProxy to obtain the reliable EthCrossChainManager contract hash.
